458a1f7a3c6a6e311a0c1d23e00a4816d11c81b7
Aidan Cole <aidanjcole@gmail.com>
2018-12-27 23:34:17 -0500

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -9,15 +9,15 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
 	def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
 		
 	def test_home_page_returns_correct_html(self):
 		response = self.client.get('/')
 		
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+		html = response.content.decode('utf8')	
+		self.assertTrue(html.startswith('<html>'))
+		self.assertIn('<title>To-Do lists</title>', html)
+		self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html') 
+		self.assertTemplateUsed(response, 'home.html') 
